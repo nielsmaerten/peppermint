12f5d604afe4355d5be4271f807600804c47d920
Niels Maerten <niels@maerten.be>
2017-08-20 22:23:22 +0200

    test(peppermint): Add initialization test for Peppermint module

--- test/peppermint.test.ts
+++ test/peppermint.test.ts
@@ -5,12 +5,7 @@ import { expect, assert } from "chai"
  * Main Peppermint tests
  */
 describe("Peppermint", () => {
-  xit("should have a newImageAdded Function", () => {
-    expect(new Peppermint().newImageAdded).to.be.a("function")
-  })
-
-  xit("should have a function to add an Image to a Dropbox", () => {
-    let peppermint = new Peppermint()
-    expect(peppermint.addImageToDropbox).to.be.a("function")
+  it("should be initializable", () => {
+    expect(new Peppermint()).to.be.instanceOf(Peppermint)
   })
 })
