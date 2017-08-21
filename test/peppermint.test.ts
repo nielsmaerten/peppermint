6bdcff3c7401bb3f277de426ad43f36a1f139e66
Niels Maerten <niels@maerten.be>
2017-08-21 08:10:39 +0200

    test(main-module): Define tests and behavior for primary exposed functions
    Add 3 main functions, and tests for their existance, plus a description in comments about what each
    function should do

--- test/peppermint.test.ts
+++ test/peppermint.test.ts
@@ -5,7 +5,24 @@ import { expect, assert } from "chai"
  * Main Peppermint tests
  */
 describe("Peppermint", () => {
+  let peppermint
+  beforeAll(() => {
+    peppermint = new Peppermint()
+  })
+
   it("should be initializable", () => {
-    expect(new Peppermint()).to.be.instanceOf(Peppermint)
+    expect(peppermint).to.be.instanceOf(Peppermint)
+  })
+
+  it("should expose an onCheckReddit function", () => {
+    expect(peppermint.onCheckReddit).to.be.a("function")
+  })
+
+  it("should expose an onNewMasterImage function", () => {
+    expect(peppermint.onNewMasterImage).to.be.a("function")
+  })
+
+  it("should expose an onNewUserImage function", () => {
+    expect(peppermint.onNewUserImage).to.be.a("function")
   })
 })
