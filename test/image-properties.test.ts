47289628a1d8a826961589edbb2272224ea605c5
Niels Maerten <niels@maerten.be>
2017-08-22 20:49:02 +0200

    test(image-properties): Update imageProperties test

--- test/image-properties.test.ts
+++ test/image-properties.test.ts
@@ -6,11 +6,19 @@ import { expect } from "chai"
  */
 describe("Image Properties", () => {
   it("should get size of an image", done => {
-    getImageProperties("http://placehold.it/500.jpg")
+    let width = 500
+    let height = 120
+    let type = "jpg"
+
+    getImageProperties(`http://placehold.it/${width}x${height}.${type}`)
       .then(properties => {
         expect(properties).to.have.property("height")
         expect(properties).to.have.property("width")
         expect(properties).to.have.property("type")
+
+        expect(properties.width).to.equal(width)
+        expect(properties.height).to.equal(height)
+        expect(properties.type).to.equal(type)
         done()
       })
       .catch(fail)
