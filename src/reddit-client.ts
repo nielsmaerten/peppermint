be6cb77c9b0380dffda60fe1a4576013480653f3
Niels Maerten <niels.maerten@belfius.be>
2017-08-22 16:26:45 +0200

    feat(reddit-client): Parse response from reddit API to array of RedditPosts

--- src/reddit-client.ts
+++ src/reddit-client.ts
@@ -5,7 +5,19 @@ export default class RedditClient {
     throw new Error("Implementation pending")
   }
 
-  public static parseResponse(data: any): RedditPost[] {
-    return [new RedditPost("")]
+  public static parseResponse(payload: any): RedditPost[] {
+    if (typeof payload === "string") {
+      payload = JSON.parse(payload)
+    }
+
+    // tslint:disable-next-line:no-unexpected-multiline
+    let posts: RedditPost[] = []
+    for (let i = 0; i < payload.data.children.length; i++) {
+      let element = payload.data.children[i]
+      if (element.data.preview) {
+        posts.push(new RedditPost(element.data.preview.images[0].source.url))
+      }
+    }
+    return posts
   }
 }
--- test/reddit-client.test.ts
+++ test/reddit-client.test.ts
@@ -4,20 +4,26 @@ import { expect, assert } from "chai"
 import * as sinon from "sinon"
 
 describe("RedditClient", () => {
-  let json = {}
-
   it("should get the top posts from a subreddit", () => {
     RedditClient.getTopPosts("/r/earthporn").then(posts => {
+      // tslint:disable-next-line:no-unused-expression
       expect(posts).not.to.be.undefined
       expect(posts.length).to.be.least(1)
     })
   })
 
   it("should parse a response from reddit to posts", () => {
-    let posts = RedditClient.parseResponse(json)
+    let posts = RedditClient.parseResponse(
+      require("./reddit-test-payload").json
+    )
+    // tslint:disable-next-line:no-unused-expression
     expect(posts).not.to.be.undefined
     posts.forEach(post => {
       expect(post).to.have.property("imageUrl")
     })
+
+    expect(posts[0].imageUrl).to.equal(
+      "https://i.redditmedia.com/ycDpouFO79-fN1NVCUZUbfxBO4sUlXdB2zTDsJBHxa8.jpg?s=68041c0aaa86722789e4bed8dabe1388"
+    )
   })
 })
