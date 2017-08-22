96a47bd85bb469ba06f910c0115ec06d36e17bb4
Niels Maerten <niels@maerten.be>
2017-08-22 18:14:45 +0200

    refactor(config): Add Config class
    Config is an object exposing configuration strings from the NPM environment, with fallback defaults

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export default class Config {
+  public static subreddit = process.env.PPM_SUBREDDIT || "/r/earthporn"
+  public static redditBaseUrl = process.env.PPM_REDDIT_BASE_URL ||
+    "https://www.reddit.com"
+  public static topPostCount = parseInt(
+    process.env.PPM_TOP_POST_COUNT as string,
+    10
+  ) || 10
+}
