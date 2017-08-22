b5596c5bb6568199f4249acd2c8a1b1126a502f1
Niels Maerten <niels@maerten.be>
2017-08-22 18:20:09 +0200

    feat(reddit-client): Implement getTopPosts method
    RedditClient.getTopPosts calls the Reddit API and fetches the top posts of a specified subreddit

--- src/reddit-client.ts
+++ src/reddit-client.ts
@@ -1,8 +1,37 @@
 import RedditPost from "./reddit-post"
+import request from "request"
+import Q from "q"
+import Config from "./config"
 
 export default class RedditClient {
-  public static getTopPosts(subreddit: string): Promise<RedditPost[]> {
-    throw new Error("Implementation pending")
+  /**
+   * Fetches top posts from a subreddit, and returns them as an array of <RedditPost>
+   * @param subreddit name of the subreddit to get posts from, including /r/. Eg: /r/earthporn. Defaults to /r/earthporn if undefined
+   * @param topPostCount number of posts to get. defaults to 10
+   */
+  public static getTopPosts(
+    subreddit?: string,
+    topPostCount?: number
+  ): Q.Promise<RedditPost[]> {
+    let deferred = Q.defer<RedditPost[]>()
+
+    // build the requestUrl
+    subreddit = subreddit || Config.subreddit
+    topPostCount = topPostCount || Config.topPostCount
+    let requestUrl = `${subreddit}/top/.json?count=${topPostCount}`.toLowerCase()
+
+    // fire off the request
+    request.get(
+      requestUrl,
+      { baseUrl: Config.redditBaseUrl },
+      (error, response, body) => {
+        if (error) deferred.reject(error)
+        else deferred.resolve(this.parseResponse(body))
+      }
+    )
+
+    // return promise
+    return deferred.promise
   }
 
   public static parseResponse(payload: any): RedditPost[] {
