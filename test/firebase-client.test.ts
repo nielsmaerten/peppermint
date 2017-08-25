a3df2c9def022f695569f00815718f4a601ba727
Niels Maerten <niels@maerten.be>
2017-08-25 23:40:51 +0200

    test(firebase-client): Replace getMasterList test
    Replace getMasterList test with check whether a post exists in the list test

--- test/firebase-client.test.ts
+++ test/firebase-client.test.ts
@@ -1,10 +1,12 @@
 import FirebaseClient from "../src/firebase-client"
 import RedditPost from "../src/reddit-post"
+import FirebaseMockCreator from "./firebase-mock-creator"
 import { expect, assert } from "chai"
+import * as Q from "q"
 
 describe("Firebase Client", () => {
-  beforeAll(() => {
-    require("./firebase-mock-creator").initMockFirebase()
+  beforeAll(done => {
+    FirebaseMockCreator.initMockFirebase().then(done)
   })
 
   it("should be a singleton", () => {
@@ -14,12 +16,22 @@ describe("Firebase Client", () => {
     assert.strictEqual(one, two)
   })
 
-  it("should return a master list", done => {
-    let posts = FirebaseClient.getInstance().getMasterList().then(posts => {
-      // TODO:add mock data to pass this test
-      assert.isArray(posts)
-      posts.forEach(post => assert.instanceOf(post, RedditPost))
-      done()
+  it("should check if a post already exists in the masterlist", done => {
+    let existingPost = FirebaseMockCreator.examplePosts[0]
+    let newPost = new RedditPost("this-post-does-not-exist-in-the-masterlist")
+
+    let p1 = FirebaseClient.getInstance().postExistsInMasterList(existingPost)
+    let p2 = FirebaseClient.getInstance().postExistsInMasterList(newPost)
+
+    Q.all([p1, p2]).then(results => {
+      try {
+        assert(results[0] === true)
+        assert(results[1] === false)
+      } catch (error) {
+        fail(error)
+      } finally {
+        done()
+      }
     })
   })
 })
