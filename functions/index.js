9a7ae7eecc60cd85903868b008c479688b782845
Niels Maerten <niels@maerten.be>
2017-08-31 13:57:41 +0200

    fix(firebase-functions): Attempt to share firebaseConfig on global variable
    Attempt at solving issue #4

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,7 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
 const peppermint = require('peppermint')
 
 exports.triggerRedditUpdate = functions.https.onRequest((request, response) => {
-  if (admin.apps.length === 0) {
-    admin.initializeApp(functions.config().firebase)
-  }
+  global.peppermintFirebaseConfig = functions.config().firebase
   return peppermint.onTriggerRedditUpdate()
 });
