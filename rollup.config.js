6405f27d0c1f20bc216164f9ece8c1b965677d9e
Niels Maerten <niels@maerten.be>
2017-10-01 15:19:39 +0200

    chore(rollup): Disable node resolution

--- rollup.config.js
+++ rollup.config.js
@@ -25,21 +25,6 @@ export default {
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
 
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve({
-      // Any additional options that should be passed through
-      // to node-resolve
-      customResolveOptions: {
-         moduleDirectory: "this string has no meaning!"
-      },
-
-      // whether to prefer built-in modules (e.g. `fs`, `path`) or
-      // local ones with the same names
-      preferBuiltins: true
-    }),
-
     // Resolve source maps to the original source
     sourceMaps()
   ]
