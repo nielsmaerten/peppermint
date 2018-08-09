d404e8853b01910760bd510f9857161e23f0899c
Niels Maerten <niels@maerten.be>
2018-08-09 21:40:24 +0200

    chore(user-settings): Align parameters

--- public/src/main.js
+++ public/src/main.js
@@ -13,16 +13,16 @@ function initialize() {
 function setPreferences() {
   const firebaseSetPreferences = firebase.functions().httpsCallable('setPreferences');
 
-  const deleteAfterDays = prompt("(1/3) After how many days do you want old wallpapers to be deleted?", 30);
-  const minWidth = prompt("(2/3) What should be the minimum with of new wallpapers?", 1920);
-  const minHeight = prompt("(3/3) What should be the minimum height of new wallpapers?", 1080);
+  const prefMaxAge = +prompt("(1/3) After how many days do you want old wallpapers to be deleted?", 30);
+  const prefMinWidth = +prompt("(2/3) What should be the minimum with of new wallpapers? (in pixels)", 1920);
+  const prefMinHeight = +prompt("(3/3) What should be the minimum height of new wallpapers? (in pixels)", 1080);
 
-  firebaseSetPreferences({ deleteAfterDays, minWidth, minHeight })
+  firebaseSetPreferences({ prefMinHeight, prefMinWidth, prefMaxAge })
     .then(() =>
       alert("Got it! Your preferences have been saved.")
     )
     .catch(e => {
-      alert("Something went wrong while saving your settings. Check console for details");
+      alert("Something went wrong while saving your settings. Check console for details.");
       console.error(e);
     })
 }
--- src/eventhandlers/on-set-preferences.ts
+++ src/eventhandlers/on-set-preferences.ts
@@ -5,17 +5,23 @@ import FirebaseClient from "../clients/firebase-client"
  */
 export default async (data: any, auth: any) => {
   let firebase = FirebaseClient.GET_INSTANCE()
+  console.log("Updating settings for user", auth.uid)
+  console.log("Settings payload:", data)
 
   try {
-    const prefMinWidth = parseInt(data.minWidth, 2)
-    const prefMinHeight = parseInt(data.minHeight, 2)
-    const prefMaxAge = parseInt(data.deleteAfterDays, 2)
+    const prefMinWidth = parseInt(data.prefMinWidth, 2)
+    const prefMinHeight = parseInt(data.prefMinHeight, 2)
+    const prefMaxAge = parseInt(data.prefMaxAge, 2)
 
-    return firebase.updateUser(auth.uid, {
-      prefMinWidth,
+    const update = {
       prefMinHeight,
+      prefMinWidth,
       prefMaxAge
-    })
+    }
+
+    console.log("Submitting update to Firebase", update)
+
+    return firebase.updateUser(auth.uid, update)
   } catch (error) {
     throw new Error("Error updating settings: " + error)
   }
