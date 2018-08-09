065ffa0ea16dced3527f2b1c2cdd59fd236de7d0
Niels Maerten <niels@maerten.be>
2018-08-09 18:50:16 +0200

    feat(user-settings): Show settings button on webpage

---
+++ public/src/main.js
@@ -0,0 +1,41 @@
+function initialize() {
+  var config = {
+    apiKey: "AIzaSyCyLae8mciMzv-ul_ehTxzRf_A4Ro8Tx74",
+    authDomain: "peppermint-wallpapers.firebaseapp.com",
+    databaseURL: "https://peppermint-wallpapers.firebaseio.com",
+    projectId: "peppermint-wallpapers",
+    storageBucket: "peppermint-wallpapers.appspot.com",
+    messagingSenderId: "372016532786"
+  };
+  firebase.initializeApp(config);
+}
+
+function setPreferences() {
+  const sendPreferences = firebase.functions().httpsCallable('sendPreferences');
+
+  const deleteAfterDays = prompt("(1/3) After how many days do you want old wallpapers to be deleted?", 30);
+  const minWidth = prompt("(2/3) What should be the minimum with of new wallpapers?", 1920);
+  const minHeight = prompt("(3/3) What should be the minimum height of new wallpapers?", 1080);
+
+  sendPreferences({ deleteAfterDays, minWidth, minHeight })
+    .then(() =>
+      alert("Got it! Your preferences have been saved.")
+    )
+    .catch(e => {
+      alert("Something went wrong while saving your settings. Check console for details");
+      console.error(e);
+    })
+}
+
+(function () {
+  initialize();
+  const hash = window.location.hash;
+  if (!hash) { return; }
+
+  const i = hash.indexOf("token=");
+  if (i < 0) { return; }
+
+  const token = hash.substr(i + 6)
+  firebase.auth().signInWithCustomToken(token);
+})()
+
