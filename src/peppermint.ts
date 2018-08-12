d6c3c3140d929a4293d4430d796e564029e7ef44
Niels Maerten <niels@maerten.be>
2018-08-13 00:10:46 +0200

    refactor(firebase-functions): Move main firebase function into root dir

--- src/peppermint.ts
+++ src/peppermint.ts
@@ -4,10 +4,51 @@ import onSetPreferences from "./eventhandlers/on-set-preferences"
 import onTriggerRedditUpdate from "./eventhandlers/on-trigger-reddit-update"
 import onUserAuthorized from "./eventhandlers/on-user-authorized"
 
-export default class Peppermint {
-  static onTriggerRedditUpdate = onTriggerRedditUpdate
-  static onNewMasterImage = onNewMasterImage
-  static onNewUserImage = onNewUserImage
-  static onUserAuthorized = onUserAuthorized
-  static onSetPreferences = onSetPreferences
-}
+import * as functions from "firebase-functions"
+;(global as any).peppermintFirebaseConfig = functions.config()
+
+exports.triggerRedditUpdate = functions.https.onRequest((request, response) => {
+  console.log("Reddit Update triggered.")
+  onTriggerRedditUpdate()
+    .then(() => {
+      // Firebase CDN will cache the response,
+      // so this function will only be executed once every 10 minutes
+      response.setHeader("Cache-Control", `public, max-age=${60 * 10}`)
+      response.sendStatus(200)
+    })
+    .catch(error => {
+      console.error(error)
+      response.sendStatus(500)
+    })
+})
+
+exports.newMasterImage = functions.database
+  .ref("masterlists/r/earthporn/{postId}")
+  .onCreate((data, context) => {
+    console.log("New master image triggered.")
+    return onNewMasterImage(data, context)
+  })
+
+exports.newUserImage = functions.database
+  .ref("users/{userId}/images/{postId}")
+  .onCreate((data, context) => {
+    console.log("New user image triggered.")
+    return onNewUserImage(data, context)
+  })
+
+exports.onUserAuthorized = functions.https.onRequest((request, response) => {
+  console.log("Connect user triggered.")
+  onUserAuthorized(request)
+    .then(url => {
+      response.redirect(url)
+    })
+    .catch(error => {
+      console.error(error)
+      response.sendStatus(500)
+    })
+})
+
+exports.setPreferences = functions.https.onCall((data, context) => {
+  console.log("Set user preferences triggered.")
+  return onSetPreferences(data, context.auth)
+})
