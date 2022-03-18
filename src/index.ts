68654e42ac6b676d2c516e75dc5a5b80297d2b3b
Niels Maerten <niels@maerten.be>
2022-03-18 13:56:40 +0100

    more boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-// Hello world
-console.log('Hello world');
+import * as functions from 'firebase-functions';
+
+// Export a Firebase Cloud function that returns 200 OK with the current time.
+export const helloWorld = functions.https.onRequest((request, response) => {
+  const currentTime = new Date().toISOString();
+  response.send(`Hello from Firebase! Current time: ${currentTime}`);
+});
